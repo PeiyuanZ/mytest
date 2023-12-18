5eac8750f79efc721a43037aace6de88a04838a5
zhangpeiyuan <peiyuan.zhang@seaboxdata.com>
2023-12-18 23:30:23 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+
+function submitComment() {
+  var comment = (document.getElementById('comment-input') as HTMLInputElement ).value;
+            (document.getElementById('user-comment') as HTMLElement ).innerText = comment;
+}
+
+onMounted(() => {
+  
+})
+</script>
+
+<template>
+  <div class="box">
+    <h1>User Comments</h1>
+    
+    <!-- 用户评论区域 -->
+    <div id="comments">
+        <!-- 用户输入的评论 -->
+        <p id="user-comment"></p>
+    </div>
+
+    <!-- 用户评论输入框 -->
+    <textarea id="comment-input" placeholder="Leave a comment..."></textarea>
+    <button @click="submitComment">Submit Comment</button>
+  </div>
+</template>
+
+<style scoped>
+.box {
+  width: 10px;
+  height: 10px;
+  background-color: aqua;
+}
+</style>
